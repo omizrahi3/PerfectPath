4d67cc62c4d0a1969dd1490459378394806c597b
racmartin62 <racmartin62@gmail.com>
2017-03-22 08:54:55 -0700

    Fixed crash on generate without starting location

---
+++ PerfectPath/PathNavigationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PathNavigationViewController.swift
+//  PerfectPath
+//
+//  Created by Rachel Martin on 3/21/17.
+//  Copyright © 2017 PerfectPath. All rights reserved.
+//
+
+import Foundation
