818184c2cd1fd88bc960f63e32b5806bae5de821
racmartin62 <racmartin62@gmail.com>
2017-04-13 16:49:25 -0400

    exercise stats and printing 'save path to favorites'

--- PerfectPath/PathNavigationViewController.swift
+++ PerfectPath/PathNavigationViewController.swift
@@ -212,13 +212,6 @@ class PathNavigationViewController: UIViewController, CLLocationManagerDelegate,
         }
     }
     
-    /*@IBAction func unwindCheckInView(segue: UIStoryboardSegue) {
-        print("In unwindCheckInView...")
-        if let svc = segue.source as? SetCheckInViewController {
-            self.path = svc.path
-            self.guardianInfo = svc.guardianInfo
-        }
-    }*/
     
     /**
      When user clicks start or resume button, start tracking location and exercise timer
@@ -245,8 +238,10 @@ class PathNavigationViewController: UIViewController, CLLocationManagerDelegate,
             } else {
                 destViewController.isPaused = false
             }
-        }
-        if segue.identifier == "GuardianPopUpViewController" {
+        } else if segue.identifier == "PathCompletedViewController" {
+            let destViewController2 : PathCompletedViewController = segue.destination as! PathCompletedViewController
+            destViewController2.path = path
+        } else if segue.identifier == "GuardianPopUpViewController" {
             let destViewController : GuardianPopUpViewController = segue.destination as! GuardianPopUpViewController
             destViewController.guardianInfo = guardianInfo
             destViewController.path = path
