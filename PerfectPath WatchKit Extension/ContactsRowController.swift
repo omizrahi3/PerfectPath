dbbfc1bae6023f7de882f5d17cac8d638c8322d0
Kasey Clark <kaseyalbers@gmail.com>
2017-04-14 15:13:28 -0400

    watch shows contacts now - but go to contacts page on phone first

---
+++ PerfectPath WatchKit Extension/ContactsRowController.swift
@@ -0,0 +1,16 @@
+//
+//  ContactsRowController.swift
+//  PerfectPath
+//
+//  Created by Kasey Clark on 4/14/17.
+//  Copyright © 2017 PerfectPath. All rights reserved.
+//
+
+import WatchKit
+
+class ContactsRowController: NSObject {
+
+    @IBOutlet var nameLabel: WKInterfaceLabel!
+    @IBOutlet var numberLabel: WKInterfaceLabel!
+
+}
