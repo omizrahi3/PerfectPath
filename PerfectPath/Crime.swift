f5965cfb9b0f596ccd8b5debb5c7365c3c8214c3
Odell Mizrahi <odellm81@live.com>
2017-04-15 13:44:14 -0400

    added date and time of day to crime pins

--- PerfectPath/Crime.swift
+++ PerfectPath/Crime.swift
@@ -14,17 +14,23 @@ class Crime: NSObject {
     var key: String?
     var crimetype: String!
     var location: String!
+    var dateof: String!
+    var shift: String!
     var x_coord: NSNumber!
     var y_coord: NSNumber!
     
     let ctKey = "crime_type"
     let lnKey = "location"
+    let doKey = "occur_date"
+    let shKey = "shift"
     var xKey = "x"
     var yKey = "y"
     
-    init (fullname: String!, phonenumber: String!, x_coordinate: NSNumber!, y_coordinate: NSNumber!) {
+    init (fullname: String!, phonenumber: String!, dateOf: String!, shift: String! ,x_coordinate: NSNumber!, y_coordinate: NSNumber!) {
         self.crimetype = fullname
         self.location = phonenumber
+        self.dateof = dateOf
+        self.shift = shift
         self.x_coord = x_coordinate
         self.y_coord = y_coordinate
     }
@@ -35,13 +41,15 @@ class Crime: NSObject {
         
         self.crimetype = dictionary![ctKey] as? String ?? ""
         self.location = dictionary![lnKey] as? String ?? ""
+        self.dateof = dictionary![doKey] as? String ?? ""
+        self.shift = dictionary![shKey] as? String ?? ""
         self.x_coord = dictionary![xKey] as? NSNumber
         self.y_coord = dictionary![yKey] as? NSNumber
         //print(self.y_coord)
     }
     
     func getSnapshotValue() -> NSDictionary {
-        let valueDict = [ctKey: crimetype, lnKey: location, xKey: x_coord, yKey: y_coord] as? NSDictionary
+        let valueDict = [ctKey: crimetype, lnKey: location, doKey: dateof, shKey: shift ,xKey: x_coord, yKey: y_coord] as? NSDictionary
         return valueDict!
     }
 }
