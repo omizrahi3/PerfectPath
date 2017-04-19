5f81924539c91631205e7c3615a17583a14c492e
racmartin62 <racmartin62@gmail.com>
2017-04-19 10:41:38 -0400

    Start, stop, guardian button, and favorite paths working with Mapbox

--- PerfectPath/PathNavigationViewController.swift
+++ PerfectPath/PathNavigationViewController.swift
@@ -50,15 +50,41 @@ class PathNavigationViewController: UIViewController, CLLocationManagerDelegate,
     var heading : CLLocationDirection?
     
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        if (returningFromSetCheckInViewController == 1) {
+            start()
+        }
         
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.activityType = .fitness
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        let status = CLLocationManager.authorizationStatus()
+        if status == .notDetermined || status == .denied || status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+
+        // if guardian is set
+        if (guardianInfo[0] == 1) {
+            print("Guardian Started")
+            // change color of btn
+            self.guardianBtn.backgroundColor = (UIColor.green)
+            self.guardianBinaryCount = testTenSecondBinary
+            //start timer
+            self.startGuardianTimer()
+        }
+//        if (comingFromFavoritePath == 1) {
+//            waypoints = (path?.mapItemWaypoints)!
+//            calculateSegmentDirections(index: 0, time: 0, routes: [])
+//        }
+
         var arr = [Waypoint]()
-        
         for waypoint in (path?.mapItemWaypoints)! {
             arr.append(Waypoint(coordinate: CLLocationCoordinate2D(latitude: waypoint.placemark.coordinate.latitude, longitude: waypoint.placemark.coordinate.longitude), name: "Mapbox"))
         }
-        
-        
         let options = RouteOptions(waypoints: arr)//, profileIdentifier: .automobileAvoidsTraffic)
         options.includesSteps = true
         
@@ -70,40 +96,156 @@ class PathNavigationViewController: UIViewController, CLLocationManagerDelegate,
             
             //            self.present(viewController, animated: true, completion: nil)
         }
+    }
+    
         
-//        if (returningFromSetCheckInViewController == 1) {
-//            start()
-//        }
-//        
+    //change button from start -> pause -> resume
+    @IBAction func startPauseButtonClicked(_ sender: Any) {
+        let buttonName = startPauseButton.titleLabel?.text
+        if buttonName == "Start" || buttonName == "Resume" {
+            if buttonName == "Start" {
+                path?.secondsTraveled = 0; path?.metersTraveled = 0.0
+            }
+            start()
+        } else {
+            startPauseButton.setTitle("Resume", for: .normal)
+            locationManager.stopUpdatingLocation()
+            //mapView.showsUserLocation = false
+            timer.invalidate()
+        }
+    }
+
+    
+    /**
+     When user clicks start or resume button, start tracking location and exercise timer
+     */
+    func start() {
+        //mapView.showsUserLocation = true
+        startPauseButton.setTitle("Pause", for: .normal)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.eachSecond(timer:)), userInfo: nil, repeats: true)
+        //mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
+        //locationManager.headingFilter = 15
+        locationManager.startUpdatingLocation()
+        //locationManager.startUpdatingHeading()
+    }
+
+    //For every second that the exercise timer is running, update exercise metrics on path object and labels
+    func eachSecond(timer: Timer) {
+        path?.secondsTraveled! += 1
+        let seconds = Double((path?.secondsTraveled)!)
+        let secondsQuantity = HKQuantity(unit: HKUnit.second(), doubleValue: seconds)
+        timeLabel.text = "Time: " + secondsQuantity.description
+        let distanceInMiles = (path?.metersTraveled)!/1609
+        let distanceRounded = ((distanceInMiles)*100).rounded()/100
+        let distanceQuantity = HKQuantity(unit: HKUnit.mile(), doubleValue: distanceRounded)
+        distanceLabel.text = "Distance: " + distanceQuantity.description
+        let speedUnit = HKUnit.mile().unitDivided(by: HKUnit.hour())
+        let speed = ((((distanceInMiles*60*60)/seconds)*10).rounded())/10
+        let paceQuantity = HKQuantity(unit: speedUnit, doubleValue: speed)
+        paceLabel.text = "Speed: " + paceQuantity.description
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations {
+            if location.horizontalAccuracy < 20 {
+                //update distance
+                if self.locations.count > 0 {
+                    path?.metersTraveled! += location.distance(from: self.locations.last!)
+                }
+                //save location
+                self.locations.append(location)
+                /*let camLocation : CLLocationCoordinate2D = location.coordinate
+                let altitude: CLLocationDistance  = 45
+                var heading : CLLocationDirection
+                if self.heading != nil {
+                    heading = self.heading!
+                } else {
+                        heading = 0
+                    }
+                    let pitch = CGFloat(40)
+                    let camera = MKMapCamera(lookingAtCenter: camLocation, fromDistance: altitude, pitch: pitch, heading: heading)
+                    mapView.setCamera(camera, animated: true)*/
+            }
+        }
+    }
+    
+    func startGuardianTimer() {
+        print("Starting Guardian Timer")
+        guardianInternalTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countDown(guardianInternalTimer:)), userInfo: nil, repeats: true)
+    }
+
+    func countDown(guardianInternalTimer: Timer) {
+        var attemptToPresentPopUp = false
+        print("PathNavigationViewController, In countDown")
+        
+        self.guardianBinaryCount -= 0b0001
+        // if the counter reached 16, reset it to 0
+        if (self.guardianBinaryCount == 0b0000) {
+            print("binaryCount: 0")
+            
+            //stop timer when it reaches 0
+            self.guardianInternalTimer.invalidate()
+            
+            //set value of button back to light grey 85 RGB and 1.0 alpha
+            //self.guardianBtn.backgroundColor = (UIColor(red:0.66, green:0.66, blue:0.66, alpha:1.0))
+            self.guardianBtn.backgroundColor = UIColor.darkGray
+            if !attemptToPresentPopUp {
+                //trigger AlertTimerCountdownController to pop up
+                self.performSegue(withIdentifier: "GuardianPopUpViewController", sender: PathNavigationViewController.self)
+            }
+            attemptToPresentPopUp = true
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SetCheckInViewController" {
+            let destViewController : SetCheckInViewController = segue.destination as! SetCheckInViewController
+            destViewController.guardianInfo = guardianInfo
+            destViewController.path = path
+            destViewController.locationManager = locationManager
+            destViewController.returningFromSetCheckInViewController = returningFromSetCheckInViewController
+            if (startPauseButton.titleLabel?.text == "Start" || startPauseButton.titleLabel?.text == "Resume") {
+                destViewController.isPaused = true
+            } else {
+                destViewController.isPaused = false
+            }
+        } else if segue.identifier == "PathCompletedViewController" {
+            let destViewController2 : PathCompletedViewController = segue.destination as! PathCompletedViewController
+            destViewController2.path = path
+        } else if segue.identifier == "GuardianPopUpViewController" {
+            let destViewController : GuardianPopUpViewController = segue.destination as! GuardianPopUpViewController
+            destViewController.guardianInfo = guardianInfo
+            destViewController.path = path
+            if (startPauseButton.titleLabel?.text == "Start" || startPauseButton.titleLabel?.text == "Resume") {
+                destViewController.isPaused = true
+            } else {
+                destViewController.isPaused = false
+            }
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+
 //        //set up location manager to track user
-//        locationManager = CLLocationManager()
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        locationManager.delegate = self
-//        locationManager.activityType = .fitness
-//        locationManager.distanceFilter = kCLDistanceFilterNone
-//        let status = CLLocationManager.authorizationStatus()
-//        if status == .notDetermined || status == .denied || status == .authorizedWhenInUse {
-//            locationManager.requestAlwaysAuthorization()
-//            locationManager.requestWhenInUseAuthorization()
-//        }
 //        mapView.delegate = self
 //        mapView.mapType = MKMapType(rawValue: 0)!
-//        if (comingFromFavoritePath == 1) {
-//            waypoints = (path?.mapItemWaypoints)!
-//            calculateSegmentDirections(index: 0, time: 0, routes: [])
-//        }
-//        
+//
 //        self.showRoute(routes: (path?.routes)! as! [MKRoute])
 //        
-//        // if guardian is set
-//        if (guardianInfo[0] == 1) {
-//            print("Guardian Started")
-//            // change color of btn
-//            self.guardianBtn.backgroundColor = (UIColor.green)
-//            self.guardianBinaryCount = testTenSecondBinary
-//            //start timer
-//            self.startGuardianTimer()
-//        }
 //        for waypoint in (path?.mapItemWaypoints)! {
 //            print("Latitude: ", waypoint.placemark.coordinate.latitude, "  Longitude: ", waypoint.placemark.coordinate.longitude)
 //        }
@@ -155,36 +297,6 @@ class PathNavigationViewController: UIViewController, CLLocationManagerDelegate,
 //        })
 //    }
 //
-//    
-//    
-//    func startGuardianTimer() {
-//        print("Starting Guardian Timer")
-//        guardianInternalTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countDown(guardianInternalTimer:)), userInfo: nil, repeats: true)
-//    }
-//    
-//    func countDown(guardianInternalTimer: Timer) {
-//        var attemptToPresentPopUp = false
-//        print("PathNavigationViewController, In countDown")
-//        
-//        self.guardianBinaryCount -= 0b0001
-//        // if the counter reached 16, reset it to 0
-//        if (self.guardianBinaryCount == 0b0000) {
-//            print("binaryCount: 0")
-//            
-//            //stop timer when it reaches 0
-//            self.guardianInternalTimer.invalidate()
-//            
-//            //set value of button back to light grey 85 RGB and 1.0 alpha
-//            //self.guardianBtn.backgroundColor = (UIColor(red:0.66, green:0.66, blue:0.66, alpha:1.0))
-//            self.guardianBtn.backgroundColor = UIColor.darkGray
-//            if !attemptToPresentPopUp {
-//                //trigger AlertTimerCountdownController to pop up
-//                self.performSegue(withIdentifier: "GuardianPopUpViewController", sender: PathNavigationViewController.self)
-//            }
-//            attemptToPresentPopUp = true
-//        }
-//    }
-//    
 //    func showRoute(routes: [MKRoute]) {
 //        for i in 0..<routes.count {
 //            plotPolyline(route: routes[i])
@@ -203,54 +315,6 @@ class PathNavigationViewController: UIViewController, CLLocationManagerDelegate,
 //            mapView.setVisibleMapRect(polylinesBoundingRect, edgePadding: UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0), animated: false)
 //        }
 //        
-//    }
-//
-//    //For every second that the exercise timer is running, update exercise metrics on path object and labels
-//    func eachSecond(timer: Timer) {
-//        path?.secondsTraveled! += 1
-//        let seconds = Double((path?.secondsTraveled)!)
-//        let secondsQuantity = HKQuantity(unit: HKUnit.second(), doubleValue: seconds)
-//        timeLabel.text = "Time: " + secondsQuantity.description
-//        let distanceInMiles = (path?.metersTraveled)!/1609
-//        let distanceRounded = ((distanceInMiles)*100).rounded()/100
-//        let distanceQuantity = HKQuantity(unit: HKUnit.mile(), doubleValue: distanceRounded)
-//        distanceLabel.text = "Distance: " + distanceQuantity.description
-//        let speedUnit = HKUnit.mile().unitDivided(by: HKUnit.hour())
-//        let speed = ((((distanceInMiles*60*60)/seconds)*10).rounded())/10
-//        let paceQuantity = HKQuantity(unit: speedUnit, doubleValue: speed)
-//        paceLabel.text = "Speed: " + paceQuantity.description
-//    }
-//    
-//    override func viewWillAppear(_ animated: Bool) {
-//        locationManager.requestAlwaysAuthorization()
-//    }
-//    
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//    }
-//    
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        for location in locations {
-//            if location.horizontalAccuracy < 20 {
-//                //update distance
-//                if self.locations.count > 0 {
-//                    path?.metersTraveled! += location.distance(from: self.locations.last!)
-//                }
-//                //save location
-//                self.locations.append(location)
-//                let camLocation : CLLocationCoordinate2D = location.coordinate
-//                let altitude: CLLocationDistance  = 45
-//                var heading : CLLocationDirection
-//                if self.heading != nil {
-//                    heading = self.heading!
-//                } else {
-//                    heading = 0
-//                }
-//                let pitch = CGFloat(40)
-//                let camera = MKMapCamera(lookingAtCenter: camLocation, fromDistance: altitude, pitch: pitch, heading: heading)
-//                mapView.setCamera(camera, animated: true)
-//            }
-//        }
 //    }
 //    
 //    //location manager for updated headings
@@ -275,65 +339,3 @@ class PathNavigationViewController: UIViewController, CLLocationManagerDelegate,
 //        }
 //        return nil
 //    }
-//
-//    //change button from start -> pause -> resume
-//    @IBAction func startPauseButtonClicked(_ sender: Any) {
-//        let buttonName = startPauseButton.titleLabel?.text
-//        if buttonName == "Start" || buttonName == "Resume" {
-//            if buttonName == "Start" {
-//                path?.secondsTraveled = 0; path?.metersTraveled = 0.0
-//            }
-//            start()
-//        } else {
-//            startPauseButton.setTitle("Resume", for: .normal)
-//            locationManager.stopUpdatingLocation()
-//            mapView.showsUserLocation = false
-//            timer.invalidate()
-//        }
-//    }
-//    
-//    
-//    /**
-//     When user clicks start or resume button, start tracking location and exercise timer
-//     */
-//    func start() {
-//        mapView.showsUserLocation = true
-//        startPauseButton.setTitle("Pause", for: .normal)
-//        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.eachSecond(timer:)), userInfo: nil, repeats: true)
-//        //mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
-//        locationManager.headingFilter = 15
-//        locationManager.startUpdatingLocation()
-//        locationManager.startUpdatingHeading()
-//    }
-//    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if segue.identifier == "SetCheckInViewController" {
-//            let destViewController : SetCheckInViewController = segue.destination as! SetCheckInViewController
-//            destViewController.guardianInfo = guardianInfo
-//            destViewController.path = path
-//            destViewController.locationManager = locationManager
-//            destViewController.returningFromSetCheckInViewController = returningFromSetCheckInViewController
-//            if (startPauseButton.titleLabel?.text == "Start" || startPauseButton.titleLabel?.text == "Resume") {
-//                destViewController.isPaused = true
-//            } else {
-//                destViewController.isPaused = false
-//            }
-//        } else if segue.identifier == "PathCompletedViewController" {
-//            let destViewController2 : PathCompletedViewController = segue.destination as! PathCompletedViewController
-//            destViewController2.path = path
-//        } else if segue.identifier == "GuardianPopUpViewController" {
-//            let destViewController : GuardianPopUpViewController = segue.destination as! GuardianPopUpViewController
-//            destViewController.guardianInfo = guardianInfo
-//            destViewController.path = path
-//            if (startPauseButton.titleLabel?.text == "Start" || startPauseButton.titleLabel?.text == "Resume") {
-//                destViewController.isPaused = true
-//            } else {
-//                destViewController.isPaused = false
-//            }
-//        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-}
